db4b180fc5f38f5ab17c7a41960448c94deeb4ac
ksoman <karthi.soman@gmail.com>
2020-01-28 12:37:34 -0800

    Added virtual env to shell and matplotlib to python script

--- hello.sh
+++ hello.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd /wynton/home/baranzinilab/karthiksoman/test_wynton
+. bin/activate
 python3 hello_world.py
--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 print("Hello world. Its a nice day!")
 print("How are you?")
 print("This is from vim")
 print("Once again a change")
-a = np.random.random((10,))
+a = np.random.random((2,))
 print(a)
+plt.plot(a)
+plt.show()
