2cb9e31a55190b920057cf8b2213f4889f147fd3
ksoman <karthi.soman@gmail.com>
2020-01-30 15:22:54 -0800

    added scratch path copy commands

--- test_run.sh
+++ test_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
 cd /wynton/home/baranzinilab/karthiksoman/test_wynton
-. bin/activate 
-python3 test_compute_node_job.py
+. bin/activate
+
+filename="text_file.txt" 
+python3 test_compute_node_job.py $filename
+
+cd $TMPDIR
+mv $filename ~/test_wynton 
