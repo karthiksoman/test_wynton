ad94edf4510ab0a0ee4c52a7654923fcb0fc409f
ksoman <karthi.soman@gmail.com>
2020-01-30 15:21:59 -0800

    added local scratch path

--- test_compute_node_job.py
+++ test_compute_node_job.py
@@ -1,21 +1,22 @@
 import os 
 import sys
+
 a = "Hello"
 b = "I am Karthik"
 c = [a, b]
 
 #global scratch path
 #scratch_path = '/wynton/scratch/ksoman/'
+#filename = scratch_path+sys.argv[1]
 
 #local scratch path
 scratch_path = os.environ['TMPDIR']
 filename = scratch_path+'/'+sys.argv[1]
-#print(filename)
-#filename = scratch_path+'test_compute_node_job_out.txt'
 
 out = open(filename, 'w')
 
 for i in range(len(c)):
     out.write(c[i]+'\n')
+
 out.close()
 
