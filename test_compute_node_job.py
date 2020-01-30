969a38e1b1912c2a79b9fd7f43eaa31c69e6212f
ksoman <karthi.soman@gmail.com>
2020-01-30 14:07:48 -0800

    added enviornment variables

--- test_compute_node_job.py
+++ test_compute_node_job.py
@@ -1,15 +1,21 @@
+import os 
+import sys
 a = "Hello"
 b = "I am Karthik"
 c = [a, b]
 
-scratch_path = '/wynton/scratch/ksoman/'
-# scratch_path = '/Users/ksoman/Desktop/'
-filename = scratch_path+'test_compute_node_job_out.txt'
+#global scratch path
+#scratch_path = '/wynton/scratch/ksoman/'
+
+#local scratch path
+scratch_path = os.environ['TMPDIR']
+filename = scratch_path+'/'+sys.argv[1]
+#print(filename)
+#filename = scratch_path+'test_compute_node_job_out.txt'
 
 out = open(filename, 'w')
 
 for i in range(len(c)):
     out.write(c[i]+'\n')
 out.close()
-
 
