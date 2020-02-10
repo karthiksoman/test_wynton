329b2b4f1784c9f81f13133cf0b5ffd12945e2a4
ksoman <karthi.soman@gmail.com>
2020-02-10 13:48:13 -0800

    testing if git works after changing the file to a different folder

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,7 @@ print("Hello world. Its a nice day!")
 print("How are you?")
 print("This is from vim")
 print("Once again a change")
+print('testing if ok after changing the file to a different folder')
 a = np.random.random((2,))
 print(a)
 
